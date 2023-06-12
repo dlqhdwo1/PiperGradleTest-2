8986fcae11b0e70fccadf025a6e01ad3d7e9633a
JungGue <star5436@naver.com>
2023-06-12 10:46:46 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import img1 from './yujung.jpg.png';
+const App = () => {
+
+  const [message,setMessage] = useState([]);
+  const [data,setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/hello')
+    .then(res => { return res.json()})
+    .then(function(data){
+      setMessage(data);
+    });
+
+    axios.get('/list').then(
+      res => {
+        setData(res.data);
+      }
+    )  
+  }, []);
+  
+  return (
+    <div className="App">
+      <header>
+        <img src={img1} style = {{width:'200px' }}/>
+        <h1>Test-10</h1>
+        <ul style = {{listStyle: 'none'}}>
+          {
+         message.map((item,index) => <li key = {index}>
+            {item}
+
+          
+          </li>)
+          }
+        </ul>
+        <hr/>
+
+        <section>
+            <h2>스프링 부트로 /list부터 받은 데이터 출력</h2>
+            {
+              data.map((item,index) => <li key = {index}>
+                {item.name} : {item.addr} : {item.age}
+
+
+              </li>)
+            }
+
+
+        </section>
+
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main/java/com/example/demo/PiperGradleTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiperGradleTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiperGradleTest2Application.class, args);
+	}
+
+}
